eceb1c52ff1784b6d4b85f8ec1a4b96facee15e3
Nidia Yáñez <niditta8@gmail.com>
2024-01-17 22:58:04 -0300

    funcionalidad carrito

--- miarchivo.js
+++ miarchivo.js
@@ -62,7 +62,7 @@ function ingreso() {
     let nombre = prompt("ingresa tu nombre y apellido")
     console.log(nombre + " ha ingresado")
 }
-
+//agregar cosas al navbar 
 let agregados = document.getElementById("agregados")
 
 let botonAgregar = document.getElementById("botonAgregar")
@@ -74,3 +74,117 @@ agregados.addEventListener ("clickr", function () {
     btn.textContent = resultado;
 });
 
+//carro de compras
+function guardarAlmacenamientoLocal(llave, valor_a_guardar) {
+    localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
+}
+function obtenerAlmacenamientoLocal(llave) {
+    const datos = JSON.parse(localStorage.getItem(llave))
+    return datos
+}
+let productos = obtenerAlmacenamientoLocal('productos') || [];
+
+const informacionAgenda = document.getElementById('informacionAgenda');
+const cardAgenda = document.getElementById('cardAgenda');
+const serviciosAgenda = document.getElementById('serviciosAgenda');
+const card = document.getElementById('card');
+const canasta = document.getElementById('canasta');
+const numero = document.getElementById("numero");
+const header = document.querySelector("#header");
+const total = document.getElementById('total');
+const body = document.querySelector("body");
+const x = document.getElementById('x')
+
+let lista = []
+let valortotal = 0
+
+window.addEventListener("scroll", function () {
+    if (card.getBoundingClientRect().top < 10) {
+        header.classList.add("scroll")
+    }
+    else {
+        header.classList.remove("scroll")
+    }
+})
+
+window.addEventListener('load', () => {
+    visualizarProductos();
+    cardAgenda.classList.add("none")
+})
+
+function visualizarProductos() {
+    card.innerHTML = ""
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].existencia > 0) {
+            card.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><button onclick=comprar(${i})>Comprar</button></div></div>`
+        }
+        else {
+            card.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><p class="soldOut">Sold Out</p></div></div>`
+        }
+    }
+}
+function comprar(indice) {
+    lista.push({ nombre: productos[indice].nombre, precio: productos[indice].valor })
+
+    let van = true
+    let i = 0
+    while (van == true) {
+        if (productos[i].nombre == productos[indice].nombre) {
+            productos[i].existencia -= 1
+            if (productos[i].existencia == 0) {
+                visualizarProductos()
+            }
+            van = false
+        }
+        guardarAlmacenamientoLocal("productos", productos)
+        i += 1
+    }
+    numero.innerHTML = lista.length
+    numero.classList.add("diseñoNumero")
+    return lista
+}
+canasta.addEventListener("click", function(){
+    body.style.overflow = "hidden"
+    cardAgenda.classList.remove('none')
+    cardAgenda.classList.add('cardAgenda')
+    serviciosAgenda.classList.add('servicioAgenda')
+    mostrarElemtrosLista()
+})
+
+function mostrarElemtrosLista() {
+    serviciosAgenda.innerHTML = ""
+    valortotal = 0
+    for (let i = 0; i < lista.length; i++){
+        serviciosAgenda.innerHTML += `<div><div class="img"><button onclick=eliminar(${i}) class="botonTrash"><img src="./recursos/trash.png"></button><p>${lista[i].nombre}</p></div><p> $${lista[i].precio}</p></div>`
+        valortotal += parseInt(lista[i].precio)
+    }
+    total.innerHTML = `<p>Valor Total</p> <p><span>$${valortotal}</span></p>`
+}
+
+function eliminar(indice){
+    let van = true
+    let i = 0
+    while (van == true) {
+        if (productos[i].nombre == lista[indice].nombre) {
+            productos[i].existencia += 1
+            lista.splice(indice, 1)
+            van = false
+        }
+        i += 1
+    }
+    guardarAlmacenamientoLocal("productos", productos)
+
+    numero.innerHTML = lista.length
+    if (lista.length == 0){
+        numero.classList.remove("diseñoNumero")
+    }
+    visualizarProductos()
+    mostrarElemtrosLista()
+}
+
+x.addEventListener("click", function(){
+    body.style.overflow = "auto"
+    cardAgenda.classList.add('none')
+    cardAgenda.classList.remove('cardAgenda')
+    serviciosAgenda.classList.remove('serviciosAgenda')
+})
